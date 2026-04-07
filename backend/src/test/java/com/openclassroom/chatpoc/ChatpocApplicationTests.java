1dfdf75bd30649c5d2adc8c244d977983b8efb9c
MakhloufiAdnan <makhloufiadnan@gmail.com>
2026-04-08 01:28:17 +0200

    chore: initialize spring boot backend

---
+++ backend/src/test/java/com/openclassroom/chatpoc/ChatpocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassroom.chatpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatpocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
